e78043a009b85e59c4b511b9530a12347559edf6
austinjung <austinjung@live.ca>
2019-11-27 14:35:42 -0800

    Add tags update tests

--- microservices/specialist_lexicon/build_spcialist_lexicon.py
+++ microservices/specialist_lexicon/build_spcialist_lexicon.py
@@ -124,10 +124,12 @@ if __name__ == '__main__':
     assert (token_dict['treatment'] == 2)
     assert (token_dict['right'] == 3)
     specialist_lexicon = AustinSimpleParser()
+    # build dictionary with tags
     specialist_lexicon.build_trie('cancer', tags={'snomed_tag': 'disorder'})
     specialist_lexicon.build_trie('breast cancer', tags={'snomed_tag': 'disorder'})
     specialist_lexicon.build_trie('right breast cancer', tags={'snomed_tag': 'disorder'})
     specialist_lexicon.build_trie('breast cancer treatment', tags={'snomed_tag': 'treatment'})
+    # Parse sentence
     parsed1 = specialist_lexicon.parse_words('cancer')
     assert (parsed1 == [('cancer', {'snomed_tag': 'disorder'})])
     parsed2 = specialist_lexicon.parse_words('breast cancer')
@@ -147,3 +149,35 @@ if __name__ == '__main__':
     assert (parsed8 == [('i', {}), ('had', {}), ('a', {}), ('breast cancer', {'snomed_tag': 'disorder'}),
                         ('treatments', {}), ('and', {}), ('cancer', {'snomed_tag': 'disorder'}), ('test', {})])
     assert (specialist_lexicon.token_dict == list(specialist_lexicon.children_tries.values())[0].token_dict)
+    # Tags update
+    specialist_lexicon.build_trie('cancer', tags={'entity_type': 'disease', 'pipeline': 'cancer'})
+    specialist_lexicon.build_trie('breast cancer', tags={'entity_type': 'disease', 'pipeline': 'cancer'})
+    specialist_lexicon.build_trie('right breast cancer', tags={'entity_type': 'disease', 'pipeline': 'cancer'})
+    specialist_lexicon.build_trie('breast cancer treatment', tags={'snomed_tag': None, 'entity_type': 'chemotherapy'})
+    # Parse sentence again
+    parsed1 = specialist_lexicon.parse_words('cancer')
+    assert (parsed1 == [('cancer', {'snomed_tag': 'disorder', 'entity_type': 'disease', 'pipeline': 'cancer'})])
+    parsed2 = specialist_lexicon.parse_words('breast cancer')
+    assert (parsed2 == [('breast cancer', {'snomed_tag': 'disorder', 'entity_type': 'disease', 'pipeline': 'cancer'})])
+    parsed3 = specialist_lexicon.parse_words('a breast cancer')
+    assert (parsed3 == [('a', {}),
+                        ('breast cancer', {'snomed_tag': 'disorder', 'entity_type': 'disease', 'pipeline': 'cancer'})])
+    parsed4 = specialist_lexicon.parse_words('have a breast cancer')
+    assert (parsed4 == [('have', {}), ('a', {}),
+                        ('breast cancer', {'snomed_tag': 'disorder', 'entity_type': 'disease', 'pipeline': 'cancer'})])
+    parsed5 = specialist_lexicon.parse_words('I have a breast cancer')
+    assert (parsed5 == [('i', {}), ('have', {}), ('a', {}),
+                        ('breast cancer', {'snomed_tag': 'disorder', 'entity_type': 'disease', 'pipeline': 'cancer'})])
+    parsed6 = specialist_lexicon.parse_words('I have a breast cancer treatment')
+    assert (parsed6 == [('i', {}), ('have', {}), ('a', {}),
+                        ('breast cancer treatment', {'entity_type': 'chemotherapy'})])
+    parsed7 = specialist_lexicon.parse_words('I have a breast cancer treatments')
+    assert (parsed7 == [('i', {}), ('have', {}), ('a', {}),
+                        ('breast cancer', {'snomed_tag': 'disorder', 'entity_type': 'disease', 'pipeline': 'cancer'}),
+                        ('treatments', {})])
+    parsed8 = specialist_lexicon.parse_words('I had a breast cancer treatments and cancer test')
+    assert (parsed8 == [('i', {}), ('had', {}), ('a', {}),
+                        ('breast cancer', {'snomed_tag': 'disorder', 'entity_type': 'disease', 'pipeline': 'cancer'}),
+                        ('treatments', {}), ('and', {}),
+                        ('cancer', {'snomed_tag': 'disorder', 'entity_type': 'disease', 'pipeline': 'cancer'}),
+                        ('test', {})])
